a525f2f806ad05f04d770d74f6efcce8ad6f5797
Luke Casey <lukecasey94@gmail.com>
2017-04-03 02:56:12 +0100

    Switch to bottom navigation

--- app/src/main/java/com/ikue/japanesedictionary/activities/MainActivity.java
+++ app/src/main/java/com/ikue/japanesedictionary/activities/MainActivity.java
@@ -8,16 +8,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -32,18 +27,13 @@ import com.ikue.japanesedictionary.fragments.FavouritesFragment;
 import com.ikue.japanesedictionary.fragments.HistoryFragment;
 import com.ikue.japanesedictionary.fragments.HomeFragment;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
-    private AppBarLayout appBarLayout;
     private NavigationView navigationView;
-    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
     private SearchView searchView;
     private MenuItem searchMenuItem;
-    private FloatingActionButton fabButton;
 
     private SharedPreferences sharedPref;
 
@@ -58,24 +48,31 @@ public class MainActivity extends AppCompatActivity {
         // Get the shared preferences
         sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 
-        appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
-
         // Add Toolbar to Main Screen
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Set Viewpager for tabs
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager();
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_history:
+                                switchFragment(new HistoryFragment());
+                                return true;
+                            case R.id.action_home:
+                                switchFragment(new HomeFragment());
+                                return true;
+                            case R.id.action_favourites:
+                                switchFragment(new FavouritesFragment());
+                                return true;
+                        }
+                        return false;
+                    }
+                });
 
-        // Set Tabs inside the Toolbar
-        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-
-        // Add icons to each tab
-        tabs.getTabAt(0).setIcon(R.drawable.ic_history_white);
-        tabs.getTabAt(1).setIcon(R.drawable.ic_home_white);
-        tabs.getTabAt(2).setIcon(R.drawable.ic_star_white);
+        setStartupPage();
 
         // Create Navigation drawer and inflate
         navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -97,13 +94,13 @@ public class MainActivity extends AppCompatActivity {
 
                 switch(item.getItemId()) {
                     case R.id.nav_history_fragment:
-                        viewPager.setCurrentItem(0, true);
+                        switchFragment(new HistoryFragment());
                         break;
                     case R.id.nav_home_fragment:
-                        viewPager.setCurrentItem(1, true);
+                        switchFragment(new HomeFragment());
                         break;
                     case R.id.nav_favourites_fragment:
-                        viewPager.setCurrentItem(2, true);
+                        switchFragment(new FavouritesFragment());
                         break;
                     case R.id.nav_settings_activity:
                         startActivity(new Intent(MainActivity.this, SettingsActivity.class));
@@ -120,8 +117,29 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
 
-        fabButton = (FloatingActionButton) findViewById(R.id.fab);
+    private void setStartupPage() {
+        int defaultStartupPage = Integer.parseInt(sharedPref.getString("pref_startupPage", "1"));
+        switch(defaultStartupPage) {
+            case 0:
+                switchFragment(new HistoryFragment());
+                break;
+            case 1:
+                switchFragment(new HomeFragment());
+                break;
+            case 2:
+                switchFragment(new FavouritesFragment());
+                break;
+        }
+    }
+
+    private void switchFragment(Fragment newFragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, newFragment)
+                .addToBackStack(null)
+                .commit();
     }
 
     @Override
@@ -144,38 +162,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Set the onClick here so we can guarantee we have a searchMenuItem
-        fabButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Expand the AppBarLayout on click, so SearchView is visible
-                appBarLayout.setExpanded(true);
-
-                // Expand the SearchView
-                MenuItemCompat.expandActionView(searchMenuItem);
-            }
-        });
-
         ComponentName cn = new ComponentName(this, SearchResultActivity.class);
         searchView.setSearchableInfo(searchManager.getSearchableInfo(cn));
 
-        // Set a listener on the SearchView
-        MenuItemCompat.setOnActionExpandListener(searchMenuItem,
-                new MenuItemCompat.OnActionExpandListener() {
-                    @Override
-                    public boolean onMenuItemActionExpand(MenuItem item) {
-                        // When the SearchView is expanded, hide the FAB
-                        fabButton.hide();
-                        return true;
-                    }
-
-                    @Override
-                    public boolean onMenuItemActionCollapse(MenuItem item) {
-                        // Once the SearchView is collapsed, show the FAB again
-                        fabButton.show();
-                        return true;
-                    }
-                });
         return true;
     }
 
@@ -195,57 +184,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-    }
-
-    private void setupViewPager() {
-        Adapter adapter = new Adapter(getSupportFragmentManager());
-        adapter.addFragment(new HistoryFragment(), getString(R.string.history_fragment_title));
-        adapter.addFragment(new HomeFragment(), getString(R.string.home_fragment_title));
-        adapter.addFragment(new FavouritesFragment(), getString(R.string.favourites_fragment_title));
-        viewPager.setAdapter(adapter);
-
-        // Set default tab to the user's preference. Default is the 'Home' tab.
-        viewPager.setCurrentItem(Integer.parseInt(sharedPref.getString("pref_startupPage", "1")));
-
-        // Set the number of pages that should be retained to either side of the current page in
-        // the view hierarchy in an idle state.
-        viewPager.setOffscreenPageLimit(2);
-
-        viewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                // When we change tab, set the current tab to be selected in the NavigationDrawer
-                navigationView.getMenu().getItem(position).setChecked(true);
-            }
-        });
-    }
-
-    static class Adapter extends FragmentPagerAdapter {
-        private final List<Fragment> fragmentList = new ArrayList<>();
-        private final List<String> fragmentTitleList = new ArrayList<>();
-
-        public Adapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return fragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return fragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            fragmentList.add(fragment);
-            fragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return fragmentTitleList.get(position);
-        }
     }
 }
